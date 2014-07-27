506e70d7b78c9e3437a446ef59422e95a2fd8f9a
Melanie Plaza <melanie@Melanies-MacBook-Air.local>
2014-07-27 13:03:09 -0700

    negative filtering

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -33,12 +33,12 @@ app.controller('MainCtrl', ['$scope', 'AdminPost', 'UserPost', 'FilteredWord', f
 
     FilteredWord.query(function(words) {
       $scope.filteredwords = words;
-      console.log($scope.filteredwords);
+      // console.log($scope.filteredwords);
     });
 
     AdminPost.query(function(posts) {
       $scope.adminPosts = posts;
-      console.log($scope.adminPosts);
+      // console.log($scope.adminPosts);
     });
 
     UserPost.query(function(posts) {
@@ -46,3 +46,31 @@ app.controller('MainCtrl', ['$scope', 'AdminPost', 'UserPost', 'FilteredWord', f
     });
 
 }])
+
+app.filter('myFilter', function() {
+  console.log('filtering');
+   return function(items, filtword) {
+    var filtered = [];
+    var matchcounter = 0;
+    angular.forEach(items, function(item) {
+      matchcounter = 0;
+      var titlearray = item.title.split(" "); 
+      for( var i = 0; i< titlearray.length; i++){
+        for( var j = 0; j< filtword.length; j++){
+          if(titlearray[i] == filtword[j].negativesearch){
+            matchcounter +=1;
+            break;
+          };
+        }    
+      }
+      if(matchcounter == 0){
+        filtered.push(item);
+      }
+
+    });
+    console.log(filtered);
+    return filtered;
+
+  };
+});
+
