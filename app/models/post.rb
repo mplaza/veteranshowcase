f1e5245f6f7fcaafe0b1533db53ab0e08dbc0107
Paul Kim <paulhyunkim@gmail.com>
2014-07-27 07:09:25 -0700

    standardizing results from API calls

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,31 @@
-
+require 'json'
+require 'net/http'
+require 'nokogiri'
+require 'open-uri'
+require 'date'
 
 class Post < ActiveRecord::Base
 
 	def self.party(keywords, authors)
 		keywords = keywords.split(" OR ")
+		authors = authors.split(" OR ")
 	
 		@results = []
 
 		keywords.each do |k|
 			feedzilla(k)
 		end 
+
+		
+		
+		huffington_post
+		
 		
 		@results = @results.flatten
+
+		p "feedzilla"
+		p @results
+
 		@results
 	end
 
@@ -24,11 +38,23 @@ class Post < ActiveRecord::Base
 		
 		response = HTTParty.get search_url, auth
 		
-		@results << response.parsed_response["articles"]
+		response.parsed_response["articles"].each do |a|
+			article = { "title" => a["title"], "author" => a["author"], "publication" => a["source"], "url" => a["url"], "publish_date" => DateTime.parse(a["publish_date"]) }
+			@results << article
+		end
+
+		# @results << response.parsed_response["articles"]
 	end
 
 
-	def huffington_post(authors)
+	def self.huffington_post
+		url = "http://www.huffingtonpost.com/author/index.php?author=chris-marvin"
+		doc = Nokogiri::HTML(open(url))
+
+		doc.css("entry").each do |item|
+			article = { "title" => item.at_css("title").text, "author" => item.at_css("author").at_css("name").text, "publication" => "Huffington Post", "url" => item.at_css("link")["href"], "publish_date" => DateTime.parse(item.at_css("published").text) }
+			@results << article
+		end
 
 	end
 
--- db/schema.rb
+++ db/schema.rb
@@ -39,12 +39,14 @@ ActiveRecord::Schema.define(version: 20140727100800) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
+    t.string   "author"
     t.string   "publication"
-    t.boolean  "favorite"
+    t.boolean  "favorite",     default: false, null: false
     t.string   "url"
     t.string   "image"
-    t.boolean  "approved"
-    t.boolean  "saved"
+    t.datetime "publish_date"
+    t.boolean  "approved",     default: false, null: false
+    t.boolean  "saved",        default: false, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
